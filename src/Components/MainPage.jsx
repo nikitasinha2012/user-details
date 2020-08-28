f8bc162a6a35a412bcc63e152d9008c4929912d4
Nikita Sinha <nikitasinha@EtherLabss-MacBook-Pro.local>
2020-08-28 23:41:41 +0530

    Date and time picker feature added

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,25 +1,29 @@
+
 import React, { Component } from 'react';
 import { Card, Modal, Button } from 'antd';
-import dateFormat from 'dateformat';
-import 'antd/dist/antd.css';
+import { AppointmentPicker } from 'react-appointment-picker';
+import AvailableTimes from 'react-available-times';
 import './MainPage.css';
-import "react-datepicker/dist/react-datepicker.css";
-import TimeRange from 'react-time-range';
-import moment from 'moment';
+import 'antd/dist/antd.css';
+import DateTimeRangeContainer from 'react-advanced-datetimerange-picker'
+import { FormControl } from 'react-bootstrap'
+import moment from "moment";
+
 
 class MainPage extends Component {
     constructor() {
-        super()
+        super();
+        let now = new Date();
+        let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0));
+        let end = moment(start).add(1, "days").subtract(1, "seconds");
         this.state = {
             userDetails: [],
             selectedUserDetails: [],
-            visible: false,
-            visibleNested: false,
-            curTime: new Date(),
-            selectedDate: new Date()
+            loading: false,
+            start: start,
+            end: end,
+            text: ''
         }
-
-
     }
     componentDidMount() {
         fetch('https://reqres.in/api/users?page=2', {
@@ -34,80 +38,124 @@ class MainPage extends Component {
                 this.setState({ userDetails: data.data })
             });
     }
+    applyCallback = (startDate, endDate) => {
+        this.setState({
+            start: startDate,
+            end: endDate
+        }
+        );
+    }
 
     showModal = (id) => {
-        this.setState({
-            visible: true,
-            selectedUserDetails: this.state.userDetails.filter((x) => x.id === id),
-        });
+        fetch(`https://reqres.in/api/users/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    visible: true,
+                    selectedUserDetails: data.data
+                });
+            })
+
     };
 
-    handleOk = e => {
+    handleOk = (id,firstName,lastName,email) => {
+        const data = { id,firstName, lastName, email};
+        fetch('https://reqres.in/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        console.log('id>',id,firstName,lastName,email);
         this.setState({
             visible: false,
-            visibleNested: false
         });
     };
 
     handleCancel = e => {
+        console.log(e);
         this.setState({
             visible: false,
-            visibleNested: false
-        });
-    };
-
-    handleChange(event) {
-        this.setState({
-            selectedDate: event.target.value
-        });
-    }
-    showNestedModal = () => {
-        this.setState({
-            visibleNested: true
         });
     };
 
 
     render() {
-        const { userDetails, selectedDate, curTime } = this.state;
-        const convertedPresentDate = dateFormat(curTime, "mmm dd yyyy");
-        const convertedSelectedDate = dateFormat(selectedDate, "mmm dd yyyy");
-        console.log('user detaisl?', userDetails)
+        const { userDetails, selectedUserDetails, text } = this.state;
+        console.log('time?', moment.utc(this.state.start).format("DD-MM-yyyy"))
+        let now = new Date();
+        let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0));
+        let end = moment(start).add(1, "days").subtract(1, "seconds");
+        let ranges = {
+            "Today Only": [moment(start), moment(end)],
+            "Yesterday Only": [moment(start).subtract(1, "days"), moment(end).subtract(1, "days")],
+            "3 Days": [moment(start).subtract(3, "days"), moment(end)]
+        }
+        let local = {
+            "format": "DD-MM-YYYY HH:mm",
+            "sundayFirst": false
+        }
+        let maxDate = moment(start).add(24, "hour")
         return (
             <div className="container">
-                <input type="text" className="input-field" />
+                <input type="text"></input>
                 {
                     userDetails.map((item, index) => {
                         return (
                             <div key={index}>
-                                <div className="user-details" onClick={this.showModal} >
+                                <div className="user-details" onClick={this.showModal.bind(this, item.id)} >
                                     <img src={item.avatar} className="avatar" />
                                     <span className="user-name">{item.first_name} {item.last_name}</span>
                                 </div>
                                 <Modal
                                     title="Basic Modal"
                                     visible={this.state.visible}
-                                    onOk={this.handleOk}
+                                    onOk={this.handleOk.bind(this, item.id,item.first_name,item.last_name,item.email)}
                                     onCancel={this.handleCancel}
                                 >
-                                    <p>ID : {item.id}</p>
-                                    <p>First Name: {item.first_name}</p>
-                                    <p>Last Name: {item.last_name}</p>
-                                    <p>Email : {item.email}</p>
-                                    <TimeRange
-                                        startMoment={this.state.startTime}
-                                        endMoment={this.state.endTime}
-                                        onChange={this.returnFunction}
-                                    />
+                                    <div key={index}>
+                                        <p>ID : {selectedUserDetails.id}</p>
+                                        <p>First Name : {selectedUserDetails.first_name}</p>
+                                        <p>Last Name : {selectedUserDetails.last_name}</p>
+                                        <p>Email : {selectedUserDetails.email}</p>
+                                    </div>
+                                    <div>
+                                        <DateTimeRangeContainer
+                                            ranges={ranges}
+                                            start={this.state.start}
+                                            end={this.state.end}
+                                            local={local}
+                                            maxDate={maxDate}
+                                            applyCallback={this.applyCallback}
+                                        >
+                                            <FormControl
+                                                id="formControlsTextB"
+                                                type="text"
+                                                label="Text"
+                                                placeholder="Enter text"
+                                                vaule={text}
+                                            />
+                                        </DateTimeRangeContainer>
+                                    </div>
                                 </Modal>
                             </div>
+
+
                         )
                     })
                 }
-
             </div>
         )
+
     }
 }
 
-export default MainPage;
+export default MainPage
